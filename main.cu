857b1dd8e4feccff343f0743fedd53fac54d0116
Mark Voschezang <voschezang>
2020-04-07 11:09:09 +0200

    refactor

--- main.cu
+++ main.cu
@@ -54,11 +54,7 @@ int main() {
     double m = n * sizeof(WTYPE_cuda) * 1e-3;
     printf("Shared data (per block) (tmp): %i , i.e. %0.3f kB\n", n, m);
   }
-// #ifdef CACHE_BATCH
-//   assert(BATCH_SIZE < THREADS_PER_BLOCK);
-// #endif
   check_params();
-
   struct timespec t0, t1, t2;
 	const size_t size = N * sizeof( WTYPE );
   clock_gettime(CLOCK_MONOTONIC, &t0);
@@ -75,83 +71,7 @@ int main() {
     *v = (STYPE *) malloc(DIMS * N * sizeof(STYPE)),
     *w = (STYPE *) malloc(DIMS * N * sizeof(STYPE));
 
-  {
-
-    const double width = 0.0005; // m
-    const double dS = width * SCALE / (double) N_sqrt; // actually dS^(1/DIMS)
-       // const double dS = 0.00002;
-    // const double dS = 0.000002;
-    const double offset = 0.5 * width;
-#if defined(RANDOM_X_SPACE) || defined(RANDOM_Y_SPACE) || defined(RANDOM_Z_SPACE)
-    const double margin = 0.;
-    const double random_range = dS - 0.5 * margin;
-    curandGenerator_t generator;
-    init_random(&generator, 11235);
-#endif
-    printf("Domain: X : %f x %f, dS: %f\n", width, width, dS);
-    float *d_random, random[4 * N_sqrt];
-    cu( cudaMalloc( (void **) &d_random, 4 * N_sqrt * sizeof(float) ) );
-    for(unsigned int i = 0; i < N_sqrt; ++i) {
-      gen_random(random, d_random, 4 * N_sqrt, generator);
-      // printf("rand: %f\n", random[0]);
-      // if (i == 0) {
-      //   for (size_t j = 0; j < 10; j++)
-      //     printf("%i + %f = %f\n", j, random[j], j + random[j]);
-
-      //   for (size_t j = 0; j < 10; j++)
-      //     printf(" %.12f > %.12f _ %.12f __ %.12f\n",
-      //            (double) random[j],
-      //            (double) random[j] * (double) dS,
-      //            (double) random[j] * (double) 10 * dS,
-      //            dS * (double) ((double) 0 + random[j]));
-      // }
-      // assert(0);
-      for(unsigned int j = 0; j < N_sqrt; ++j) {
-        size_t idx = i * N_sqrt + j;
-        x[idx].x = 0; // real part
-        x[idx].y = 0; // imag part
-        if (i == N_sqrt * 1/2 && j == N_sqrt / 2) x[idx].x = 1;
-        // if (i == N_sqrt * 1/3 && j == N_sqrt / 2) x[idx] = 1;
-        // if (i == N_sqrt * 2/3 && j == N_sqrt / 2) x[idx] = 1;
-        // if (i == N_sqrt * 1/4 && j == N_sqrt / 4) x[idx] = 1;
-        // if (i == N_sqrt * 3/4 && j == N_sqrt / 4) x[idx] = 1;
-
-        u[Ix(i,j,0)] = i * dS - offset;
-        u[Ix(i,j,1)] = j * dS - offset;
-        u[Ix(i,j,2)] = 0;
-
-#ifdef RANDOM_Y_SPACE
-        v[Ix(i,j,0)] = i * dS - offset + random_range * \
-           (random[j*4+0] - 0.5);
-        v[Ix(i,j,1)] = j * dS - offset + random_range * \
-           (random[j*4+1] - 0.5);
-        if (i == 2 && j == 2) {
-          printf("rand: %f, %f; %f, %f\n", v[Ix(i,j,0)], v[Ix(i-1,j,0)], v[Ix(i,j-1,0)], v[Ix(i-1,j-1,0)]);
-        }
-#else
-        v[Ix(i,j,0)] = i * dS - offset;
-        v[Ix(i,j,1)] = j * dS - offset;
-#endif
-        v[Ix(i,j,2)] = -0.02;
-
-
-
-#ifdef RANDOM_Z_SPACE
-        w[Ix(i,j,0)] = j * dS - offset + random_range * \
-          (random[j*4+2] - 0.5);
-        w[Ix(i,j,1)] = j * dS - offset + random_range * \
-           (random[j*4+3] - 0.5);
-#else
-        w[Ix(i,j,0)] = i * dS - offset;
-        w[Ix(i,j,1)] = j * dS - offset;
-#endif
-        w[Ix(i,j,2)] = 0;
-      } }
-
-  curandDestroyGenerator(generator);
-  cu( cudaFree( d_random ) );
-  }
-
+  init_planes(x, u, v, w);
   summarize_double('u', u, N * DIMS);
   summarize_double('v', v, N * DIMS);
 
