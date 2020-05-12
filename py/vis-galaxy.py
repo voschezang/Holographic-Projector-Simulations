7ef56f2f192791f8a657533845b74bec7038c6d5
mark <voschezang>
2020-05-12 16:11:56 +0200

    fix 3d scatter and rotations

--- py/vis-galaxy.py
+++ py/vis-galaxy.py
@@ -10,9 +10,11 @@ Astronomers and cosmologists will kill me !
 """
 
 import numpy as np
+import itertools
 
 from vispy import gloo
 from vispy import app
+import vispy.scene
 from vispy.util.transforms import perspective, translate, rotate
 # local
 import util
@@ -109,22 +111,36 @@ void main()
 """
 
 
-class Canvas(app.Canvas):
+class MyCanvas(app.Canvas):
+    # class MyCanvas(vispy.scene.SceneCanvas):
 
     def __init__(self, pos=None, colors=None):
+        # pos = np.ndarray or list of np.ndarray
+        # super().__init__(self, keys='interactive', size=(800, 600))
+        # self.view = self.central_widget.add_view()
         app.Canvas.__init__(self, keys='interactive', size=(800, 600))
+        # canvas = app.Canvas(size=(800, 600), show=True, keys='interactive')
+        # gloo.set_viewport(0, 0, canvas.size[0], canvas.size[1])
+        # gloo.set_state("translucent", depth_test=False)
+
+        # self.view.camera = 'arcball'
+        # self.view.camera = 'turntable'
         # ps = self.pixel_scale
 
         self.title = "A very fake galaxy [mouse scroll to zoom]"
+        # self.unfreeze()
+        self.cycle_data = isinstance(pos, list)
 
         if pos is None:
             p = 50000
             n = p
+        elif isinstance(pos, list):
+            n = pos[0].shape[0]
         else:
             n = pos.shape[0]
 
-        data = np.zeros(n, [('a_position', np.float32, 3),
-                            ('a_dist', np.float32)])
+        self.data = np.zeros(n, [('a_position', np.float32, 3),
+                                 ('a_dist', np.float32)])
 
         i = 0
         if pos is None:
@@ -135,20 +151,31 @@ class Canvas(app.Canvas):
             pos = P
             colors = D
 
-        data['a_dist'] = colors
-        data['a_position'] = pos
+        # data['a_dist'] = colors
+        # data['a_position'] = pos
+
+        if isinstance(pos, np.ndarray):
+            colors = [colors]
+            pos = [pos]
+
+        self.colors = itertools.cycle(colors)
+        self.pos = itertools.cycle(pos)
+        # data['a_position'] = pos
+        # self.data['a_dist'] = next(self.colors)
+        # self.data['a_position'] = next(self.pos)
         # data['a_size'][(i + 0) * p:(i + 1) * p] = S * ps
 
         self.point_size = 15
         self.program = gloo.Program(VERT_SHADER, FRAG_SHADER)
         self.model = np.eye(4, dtype=np.float32)
         self.projection = np.eye(4, dtype=np.float32)
-        self.theta, self.phi = 0, 0
+        self.theta, self.phi, self.xi = 0, 0, 0
+        self.update_data()
 
         self.translate = 5
         self.view = translate((0, 0, -self.translate))
 
-        self.program.bind(gloo.VertexBuffer(data))
+        self.program.bind(gloo.VertexBuffer(self.data))
         self.program['u_colormap'] = gloo.Texture2D(cmap)
         self.program['u_size'] = self.point_size / self.translate
         self.program['u_model'] = self.model
@@ -165,6 +192,13 @@ class Canvas(app.Canvas):
 
         self.show()
 
+    def update_data(self):
+        self.data['a_dist'] = next(self.colors)
+        self.data['a_position'] = next(self.pos)
+        # data['a_dist'] = colors
+        # data['a_position'] = pos
+        self.program.bind(gloo.VertexBuffer(self.data))
+
     def on_key_press(self, event):
         if event.text == ' ':
             if self.timer.running:
@@ -173,10 +207,18 @@ class Canvas(app.Canvas):
                 self.timer.start()
 
     def on_timer(self, event):
-        self.theta += .11
-        self.phi += .13
-        self.model = np.dot(rotate(self.theta, (0, 0, 1)),
-                            rotate(self.phi, (0, 1, 0)))
+        # self.theta += .11
+        # self.phi += .13
+        self.xi += .02
+        # self.model = np.dot(rotate(self.theta, (0, 0, 1)),
+        #                     rotate(self.phi, (0, 1, 0)))
+        if self.xi > 1:
+            # precise timing is not important
+            # reset naively, no need for modulo
+            self.xi -= 1
+            if self.cycle_data:
+                self.update_data()
+
         self.program['u_model'] = self.model
         self.update()
 
@@ -191,8 +233,21 @@ class Canvas(app.Canvas):
         self.program['u_size'] = self.point_size / self.translate
         self.update()
 
+    def on_mouse_move(self, event):
+        # TODO use realist rotations
+        if event.button == 1 and event.last_event is not None:
+            w, h = self.size
+            dx, dy = event.pos - event.last_event.pos
+            rotation_speed = 100
+            self.phi += rotation_speed * dx / w
+            self.theta += rotation_speed * dy / h
+            self.model = np.dot(rotate(self.theta, (0, 0, 1)),
+                                rotate(self.phi, (0, 1, 0)))
+            self.update()
+
     def on_draw(self, event):
         gloo.clear()
+        # gloo.clear('white')
         self.program.draw('points')
 
     def apply_zoom(self):
@@ -206,19 +261,60 @@ if __name__ == '__main__':
     dir = '../tmp'
     fn = 'out.zip'
     data = util.parse_file(dir, fn, 'out.txt')
-    pos = data['v']
-    colors = data['y'][:, 0]
-    print(colors.min(), colors.max(), colors.mean())
-    colors -= colors.min()
-    colors /= colors.max()
-    print(colors.min(), colors.max(), colors.mean())
-    # print(colors.min(), colors.max())
-    # colors = np.random.normal(0, 1, size=colors.size)
-    # colors = np.linspace(0, 1, colors.size)
-    for i in range(2):
-        pos[:, i] /= pos[:, i].max()
+    viewtype = 2
+    if viewtype == 0:
+        n = 100000
+        pos = data['v'][:n]
+        colors = data['y'][:n, 0]
+        print(colors.min(), colors.max(), colors.mean())
+        colors -= colors.min()
+        colors /= colors.max()
+        print(colors.min(), colors.max(), colors.mean())
+        # print(colors.min(), colors.max())
+        # colors = np.random.normal(0, 1, size=colors.size)
+        # colors = np.linspace(0, 1, colors.size)
+        for i in range(2):
+            pos[:, i] /= pos[:, i].max()
 
-    n = 10000
-    c = Canvas(pos, colors)
-    # c = Canvas()
+    elif viewtype == 1:
+        n = 1000000
+        colors = [z[:n, 0] for z in data['z']]
+        max_color = max((c.max() for c in colors))
+        pos = [w[:n] for w in data['w']]
+        for i in range(len(colors)):
+            colors[i] /= max_color
+        # for c in colors:
+        #     c /= max_color
+
+        for p in pos:
+            for i in range(2):
+                p[:, i] /= p[:, i].max()
+
+    elif viewtype == 2:
+        n = 100000
+        m = 10
+        # normalize z_offset
+        max_z_offset = max((np.abs(w[:, -1]).max() for w in data['w'][:m]))
+        if max_z_offset > 0:
+            for w in data['w'][:m]:
+                # normalize
+                w[:, -1] /= max_z_offset
+                # center
+                w[:, -1] -= 0.5
+
+        # pos = np.vstack([data['w'][i][:n] for i in range(len(data['w']))])
+        print(len(data['z']))
+        pos = np.vstack([w[:n] for w in data['w'][:m]])
+        a = np.vstack([z[:n, :1] for z in data['z'][:m]]).reshape(-1)
+        # mu = a.mean()
+        # std = a.std()
+        # pos = np.vstack((w[:n] for w in data['w']))
+        print(a.shape, pos.shape)
+        a /= a.max()
+        for i in range(2):
+            pos[:, i] /= pos[:, i].max()
+
+        colors = a
+
+    canvas = MyCanvas(pos, colors)
     app.run()
