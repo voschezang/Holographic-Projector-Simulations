212366b599f0a5629f0c3de2227e9c2034d71b1c
mark <voschezang>
2020-05-12 16:19:19 +0200

    cleanup

--- py/vis-galaxy.py
+++ py/vis-galaxy.py
@@ -4,64 +4,14 @@
 # Copyright (c) Vispy Development Team. All Rights Reserved.
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
 
-"""
-Just a very fake galaxy.
-Astronomers and cosmologists will kill me !
-"""
-
 import numpy as np
 import itertools
-
 from vispy import gloo
 from vispy import app
 import vispy.scene
 from vispy.util.transforms import perspective, translate, rotate
 # local
 import util
-
-# Manual galaxy creation
-# (did you really expect a simulation in less than 250 python lines ?)
-
-
-def make_arm(n, angle):
-    R = np.linspace(10, 450 + 50 * np.random.uniform(.5, 1.), n)
-    R += 40 * np.random.normal(0, 2., n) * np.linspace(1, .1, n)
-    T = angle + np.linspace(0, 2.5 * np.pi, n) + \
-        np.pi / 6 * np.random.normal(0, .5, n)
-    S = 8 + 2 * np.abs(np.random.normal(0, 1, n))
-    S *= np.linspace(1, .85, n)
-    P = np.zeros((n, 3), dtype=np.float32)
-    X, Y, Z = P[:, 0], P[:, 1], P[:, 2]
-    X[...] = R * np.cos(T)
-    Y[...] = R * np.sin(T) * 1.1
-    D = np.sqrt(X * X + Y * Y)
-    Z[...] = 8 * np.random.normal(0, 2 - D / 512., n)
-    X += (D * np.random.uniform(0, 1, n) > 250) * \
-        (.05 * D * np.random.uniform(-1, 1, n))
-    Y += (D * np.random.uniform(0, 1, n) > 250) * \
-        (.05 * D * np.random.uniform(-1, 1, n))
-    Z += (D * np.random.uniform(0, 1, n) > 250) * \
-        (.05 * D * np.random.uniform(-1, 1, n))
-    D = (D - D.min()) / (D.max() - D.min())
-
-    return P / 256, S / 2, D
-
-
-# Very simple colormap
-cmap = np.array([[255, 124, 0], [255, 163, 76],
-                 [255, 192, 130], [255, 214, 173],
-                 [255, 232, 212], [246, 238, 237],
-                 [237, 240, 253], [217, 228, 255],
-                 [202, 219, 255], [191, 212, 255],
-                 [182, 206, 255], [174, 202, 255],
-                 [168, 198, 255], [162, 195, 255],
-                 [158, 192, 255], [155, 189, 255],
-                 [151, 187, 255], [148, 185, 255],
-                 [145, 183, 255], [143, 182, 255],
-                 [141, 181, 255], [140, 179, 255],
-                 [139, 179, 255],
-                 [137, 177, 255]], dtype=np.uint8).reshape(1, 24, 3)
-
 
 VERT_SHADER = """
 #version 120
@@ -76,7 +26,7 @@ uniform float u_size;
 // Attributes
 // ------------------------------------
 attribute vec3  a_position;
-attribute float a_dist;
+attribute float a_dist; // color
 
 // Varyings
 // ------------------------------------
@@ -103,9 +53,6 @@ varying float v_dist;
 // ------------------------------------
 void main()
 {
-    float a = 2*(length(gl_PointCoord.xy - vec2(0.5,0.5)) / sqrt(2.0));
-    vec3 color = texture2D(u_colormap, vec2(v_dist,.5)).rgb;
-    // gl_FragColor = vec4(color,(1-a)*.25);
     gl_FragColor = vec4(v_dist, 1-v_dist, 0, 1);
 }
 """
@@ -115,21 +62,11 @@ class MyCanvas(app.Canvas):
     # class MyCanvas(vispy.scene.SceneCanvas):
 
     def __init__(self, pos=None, colors=None):
-        # pos = np.ndarray or list of np.ndarray
-        # super().__init__(self, keys='interactive', size=(800, 600))
-        # self.view = self.central_widget.add_view()
+        """
+        pos = np.ndarray or list of np.ndarray
+        """
         app.Canvas.__init__(self, keys='interactive', size=(800, 600))
-        # canvas = app.Canvas(size=(800, 600), show=True, keys='interactive')
-        # gloo.set_viewport(0, 0, canvas.size[0], canvas.size[1])
-        # gloo.set_state("translucent", depth_test=False)
-
-        # self.view.camera = 'arcball'
-        # self.view.camera = 'turntable'
-        # ps = self.pixel_scale
-
-        self.title = "A very fake galaxy [mouse scroll to zoom]"
-        # self.unfreeze()
-        self.cycle_data = isinstance(pos, list)
+        self.title = "3D scatter plot [click & drag mouse to rotate]"
 
         if pos is None:
             p = 50000
@@ -139,20 +76,9 @@ class MyCanvas(app.Canvas):
         else:
             n = pos.shape[0]
 
+        self.cycle_data = isinstance(pos, list)
         self.data = np.zeros(n, [('a_position', np.float32, 3),
                                  ('a_dist', np.float32)])
-
-        i = 0
-        if pos is None:
-            P, S, D = make_arm(p, i * 2 * np.pi / 3)
-            print(P.shape, S.shape, D.shape)
-            print('size', S.min(), S.max())
-            print('d', D.min(), D.max())
-            pos = P
-            colors = D
-
-        # data['a_dist'] = colors
-        # data['a_position'] = pos
 
         if isinstance(pos, np.ndarray):
             colors = [colors]
@@ -160,12 +86,8 @@ class MyCanvas(app.Canvas):
 
         self.colors = itertools.cycle(colors)
         self.pos = itertools.cycle(pos)
-        # data['a_position'] = pos
-        # self.data['a_dist'] = next(self.colors)
-        # self.data['a_position'] = next(self.pos)
-        # data['a_size'][(i + 0) * p:(i + 1) * p] = S * ps
 
-        self.point_size = 15
+        self.point_size = 9
         self.program = gloo.Program(VERT_SHADER, FRAG_SHADER)
         self.model = np.eye(4, dtype=np.float32)
         self.projection = np.eye(4, dtype=np.float32)
@@ -176,7 +98,7 @@ class MyCanvas(app.Canvas):
         self.view = translate((0, 0, -self.translate))
 
         self.program.bind(gloo.VertexBuffer(self.data))
-        self.program['u_colormap'] = gloo.Texture2D(cmap)
+        # self.program['u_colormap'] = gloo.Texture2D(cmap)
         self.program['u_size'] = self.point_size / self.translate
         self.program['u_model'] = self.model
         self.program['u_view'] = self.view
